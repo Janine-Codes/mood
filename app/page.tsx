3cd32b6f3a692234d41c81f7a3dd9ef40e78f1d9
Janine-Codes <janinekrse@gmail.com>
2025-04-15 16:50:02 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white bg-black">
+      <div>
+        <h1>title</h1>
+        <p>description</p>
+        <div>
+          <button>get started</button>
+        </div>
+      </div>
+    </div>
+  )
+}
